3cddf1c896e30e4ed0c1e678336177ab16565eb2
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2021-03-18 01:27:46 -0700

    Add .dir-locals for auto format buffer.

--- src/parser.rs
+++ src/parser.rs
@@ -2,9 +2,9 @@ use async_std::io::BufRead;
 use async_std::io::BufReader;
 use async_std::net::TcpStream;
 use async_std::stream::Stream;
+// use async_std::io::BufReadExt;
+// use async_std::io::BufReadExt;
 
-// use async_std::io::BufReadExt;
-// use async_std::io::BufReadExt;
 use crate::command::Command;
 use async_std::prelude::*;
 
